5e5e2a69b540ca4a31cfe8ac7ca7de2daf56b762
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-26 17:13:19 +0530

    disable ctrl + m

--- editor.c
+++ editor.c
@@ -17,8 +17,8 @@ void enableRawMode ()
 	atexit(disableRawMode);		// diables raw mode at exit of the program
 
 	struct termios raw = orig_termios;		// to make sure that the changes don't take place in global variable
-	raw.c_lflag &= ~( ECHO | ICANON | ISIG);		// togalling canonical mode off and also tacking the problem with SIGCONT and SIGSTP
-	raw.c_iflag &= ~(IXON);		// stoping XON\OFF to pause the transmission
+	raw.c_lflag &= ~( ECHO | ICANON | ISIG | IEXTEN);		// togalling canonical mode off and also tacking the problem with SIGCONT and SIGSTP
+	raw.c_iflag &= ~(ICRNL | IXON);		// stoping XON\OFF to pause the transmission also ICRNL for ctrl-m
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);	// turning on raw mode
 }
