f2d5e99c6c4e67e4f1d738e93ac45a7697973359
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-26 16:42:14 +0530

    turn on echoing at the end of the program

--- editor.c
+++ editor.c
@@ -1,9 +1,29 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+struct termios orig_termios;
+
+void disableRawMode ()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode ()
+{
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);		// diables raw mode at exit of the program
+
+	struct termios raw = orig_termios;		// to make sure that the changes don't take place in global variable
+	raw.c_lflag &= ~( ECHO );
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);	// turning on raw mode
+}
+
 int main()
 {
+	enableRawMode();	
+
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    while (read(STDIN_FILENO, &c, 1) == 1 && c!='q');
 	return 0;
 }
