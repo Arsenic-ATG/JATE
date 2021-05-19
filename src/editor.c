c5c21ee2f893d6175ace8972741f7af118ef5d39
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2021-05-19 17:24:20 +0530

    Get file name via command line arg

--- src/editor.c
+++ src/editor.c
@@ -523,11 +523,12 @@ void init_editor()
 }
 
 /************************** main() function *************************/
-int main()
+int main(int argc, char *argv[])
 {
 	enable_raw_mode();
 	init_editor();
-	editor_open("/Users/ankursaini/Desktop/test_file.txt");
+	if (argc >= 2)
+	    editor_open(argv[1]);
 
     while (1)
     {
